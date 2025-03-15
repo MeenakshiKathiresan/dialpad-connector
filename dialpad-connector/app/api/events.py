92da177ee53db9f6a2fb485473ea51f1436b543a
meenakshikathiresan <meenakshikathiresan3@gmail.com>
2025-03-15 09:49:22 -0400

    Moved post calls to salesforce service

--- dialpad-connector/app/api/events.py
+++ dialpad-connector/app/api/events.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, HTTPException
-from services.salesforce_service import get_salesforce_token
+from fastapi import APIRouter
+from services.salesforce_service import post_call_data_to_salesforce
 from services.dial_pad_service import get_dialpad_calls
 from models.Call import Call
 import requests
@@ -8,33 +8,6 @@ router = APIRouter()
 
 @router.post("/post-call")
 async def post_call_data():
-    
     call_list: list[Call] = get_dialpad_calls()
-
-    access_token, instance_url = get_salesforce_token()
-    salesforce_url = f"{instance_url}/services/data/v58.0/sobjects/Event"
-    headers = {"Authorization": f"Bearer {access_token}", "Content-Type": "application/json"}
-
-    results = {
-        "successful": [],
-        "failed": []
-    }
-
-    for call in call_list:
-        event_data = {
-            "Subject": f"{call.subject} to {call.phone}",  
-            "ActivityDateTime": call.activity_date.isoformat(), 
-            "DurationInMinutes": call.duration_in_minutes,
-            "Description": call.phone,  
-        }
-        response = requests.post(salesforce_url, json=event_data, headers=headers)
-
-        if response.status_code == 201:
-            results["successful"].append(response.json().get("id"))
-        else:
-            results["failed"].append(response.text)
-        
-    return {
-        "message": f"{len(results)} calls posting completed.",
-        "results": results
-    }
+    return post_call_data_to_salesforce(call_list)
+    
--- dialpad-connector/app/services/salesforce_service.py
+++ dialpad-connector/app/services/salesforce_service.py
@@ -32,5 +32,32 @@ def get_salesforce_token():
     else:
         raise HTTPException(status_code=400, detail="Failed to authenticate with Salesforce")
 
+def post_call_data_to_salesforce(call_list):
+    access_token, instance_url = get_salesforce_token()
+    salesforce_url = f"{instance_url}/services/data/v58.0/sobjects/Event"
+    headers = {"Authorization": f"Bearer {access_token}", "Content-Type": "application/json"}
 
+    results = {
+        "successful": [],
+        "failed": []
+    }
 
+    for call in call_list:
+        event_data = {
+            "Subject": f"{call.subject} to {call.phone}",  
+            "ActivityDateTime": call.activity_date.isoformat(), 
+            "DurationInMinutes": call.duration_in_minutes,
+            "Description": call.phone,  
+        }
+        response = requests.post(salesforce_url, json=event_data, headers=headers)
+
+        if response.status_code == 201:
+            results["successful"].append(response.json().get("id"))
+        else:
+            results["failed"].append(response.text)
+        
+    return {
+        "message": f"{len(results)} calls posting completed.",
+        "results": results
+    }
+    
